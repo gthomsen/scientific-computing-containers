315bcc431f9f85e8925a03a1fe17257b1fd1e2d0
Greg Thomsen <gthomsen@gmail.com>
2022-12-22 13:44:03 +0000

    Topic: gfortran 7.5 + OpenMPI 4.1 December 2022 Release Initial Addition
    Description:
      * Created the 'gfortran7.5-openmpi4.1-2022.12' branch.  This is a
        functionality refresh bringing in changes from 'master as well as a security
        refresh and builds from an Ubuntu 18.04 LTS image with patches up until
        2022/11/30.

---
+++ release/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# simple script for building the gfortran7.5-openmpi4.1-2022.12 image.
+
+# parallelize the build as much as possible.
+NUMBER_CORES=`grep MHz /proc/cpuinfo  | wc -l`
+
+podman build \
+    --build-arg NUMBER_JOBS=${NUMBER_CORES} \
+    -t gfortran7.5-openmpi4.1-2022.12 \
+    ../gfortran+openmpi
